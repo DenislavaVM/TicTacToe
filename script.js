fb49f92fd6b7459daa4f2a5d44d79e64f7802997
Denislava Milanova <167196882+DenislavaVM@users.noreply.github.com>
2024-06-25 18:54:48 +0300

    Determine winner

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var startingBoard;
 const playerSymbol = 'O';
 const computerSymbol = 'X';
-const winCombos = [
+const victoryPatterns = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -31,9 +31,56 @@ function handleTurnClick(square) {
     turn(square.target.id, playerSymbol);
 }
 
-function turn(squareId, player) {
-
+function turn(squareId, currentPlayer) {
     // Update the board array and the UI
-    startingBoard[squareId] = player;
-    document.getElementById(squareId).textContent = player;
+    startingBoard[squareId] = currentPlayer;
+    let symbolNode = createHTMLElementFromString (getSymbolHTML(currentPlayer));
+    document.getElementById(squareId).innerHTML = '';
+    document.getElementById(squareId).appendChild(symbolNode);
+    let gameWon = checkWin(startingBoard, currentPlayer);
+    if (gameWon) {
+        gameOver(gameWon);
+    }
+}
+
+function getSymbolHTML(currentPlayer) {
+    if (currentPlayer === playerSymbol) {
+        return '<span class="material-symbols-outlined" style="font-size: 3rem; color: #B22222;">close</span>';
+    } else {
+        return '<span class="material-symbols-outlined" style="font-size: 3rem; color: #b26a22;">circle</span>';
+    }
+}
+
+function createHTMLElementFromString (html) {
+    let template = document.createElement('template');
+    template.innerHTML = html.trim();
+    return template.content.firstChild;
+}
+
+function checkWin(board, player) {
+    // Collect all the indexes that the player has played in
+    let plays = board.reduce((a, element, index) =>
+        (element === player) ? a.concat(index) : a, []);
+    let gameWon = null;
+
+    // Loop through the winning combinations
+    for (let [index, win] of victoryPatterns.entries()) {
+
+        // Check if the player's plays contain all elements of the winning combination
+        if (win.every(element => plays.indexOf(element) > -1)) {
+            gameWon = { index: index, player: player };
+            break;
+        }
+    }
+    return gameWon;
+}
+
+function gameOver(gameWon) {
+    for (let index of victoryPatterns[gameWon.index]) {
+        document.getElementById(index).style.backgroundColor = gameWon.player == playerSymbol ? "#f8dbdb" : "#b26a22";
+    }
+
+    for (let index = 0; index < cells.length; index++) {
+       cells[index].removeEventListener('click', handleTurnClick, false);
+    }
 }
